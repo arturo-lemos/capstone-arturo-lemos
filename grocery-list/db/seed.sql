96d93b91e3fe5e49580f116555955555d32a1f3a
Arturo <lemosart0987@gmail.com>
2025-12-15 19:54:15 -0600

    Created table for items, added 8 different categories, and seeded the table

---
+++ grocery-list/db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO items (name, category, purchased)
+VALUES
+    ('Milk', 'dairy', false),
+    ('Eggs', 'dairy', false),
+    ('Apples', 'produce', false),
+    ('Chicken Breast', 'meat', false),
+    ('Bread', 'bakery', false),
+    ('Ice Cream', 'frozen', false),
+    ('Soda', 'beverages', false),
+    ('Chips', 'snacks', false),
+    ('Batteries', 'misc', false);
